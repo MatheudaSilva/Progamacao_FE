f3dd7cf818970a29b3da9b2c5964f4824d79af0b
Matheus da Silva <matheus4589silva@gmail.com>
2022-04-28 16:01:20 -0300

    first commit

---
+++ Aula3/script3_02-1.js
@@ -0,0 +1,11 @@
+var aluno, nota;
+aluno = "Astrogildo";
+nota = "9.5";//string, conjunto de caract.
+document.write (aluno);
+document.write ("<hr>");
+document.write (nota);
+/*
+Saída:
+Astrogildo
+9.5
+*/
---
+++ Aula4/script4_01.js
@@ -0,0 +1,6 @@
+var x, y ,z;
+y=3;
+z=5;
+x=2*y+z;
+document.write("x = <b>" + x +"</b>");
+document.write("<br>Na outra linha, temos.....")
